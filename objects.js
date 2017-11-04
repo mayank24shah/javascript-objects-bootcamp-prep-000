b0b3a983ac8a7b4f6f3a72ef1ee5e24e903ade95
Mayank Shah <mayank.24shah@gmail.com>
2017-11-04 22:56:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = {coldPlay:"Scientist"}
 function updatePlaylist(playlist, artistName, songTitle){
-  return object.assign({},playlist,{[artistName]:songTitle})
+  playlist[artistName] = songTitle
+  return playlist 
 }
