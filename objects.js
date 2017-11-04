37017139b18a1aff7703f895b2d0011130f98b10
Mayank Shah <mayank.24shah@gmail.com>
2017-11-04 23:04:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist
 }
 function removeFromPlaylist(playlist, artistName){
-  var x = delete playlist.artistName;
-  return x;
+  delete playlist.artistName;
+  
 }
