57b4bfa7af7272fc509fe498dc281fac8130506b
Mayank Shah <mayank.24shah@gmail.com>
2017-11-04 22:59:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {coldPlay:"Scientist"}
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
-  return playlist 
+  return playlist
 }
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName
